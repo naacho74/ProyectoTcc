94022de30af5f0902024431b8df054de6e907f73
409 <409@academicos.cesde.edu.co>
2022-10-13 13:56:55 -0500

    ok

--- src/app/cuarto/cuarto.component.ts
+++ src/app/cuarto/cuarto.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { MercanciasService } from '../services/mercancias.service';
+import { ZonasService } from '../services/zonas.service';
 
 @Component({
   selector: 'app-cuarto',
@@ -7,7 +9,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CuartoComponent implements OnInit {
 
-  constructor() { }
+  datosZonas:any[]=[];
+
+  constructor(public servicioZona:ZonasService,
+    public servicioMercancia:MercanciasService) { 
+
+    this.servicioZona.consultarZonas()
+      .subscribe(respuesta=>{
+        console.log(respuesta);
+        this.datosZonas=respuesta
+        console.log(this.datosZonas)
+      })
+
+  }
+
+  retirarMercancia(idMercancia:any):void{
+
+    this.servicioMercancia.retirarMercancia(idMercancia)
+    .subscribe(respuesta=>{
+      console.log(respuesta)
+      window.location.reload()
+    })
+    
+  }
 
   ngOnInit(): void {
   }
